2ab16eaa578f0ac3ee4e69ed4832c23faa6892e2
Juan David <87282119+DAVI161718@users.noreply.github.com>
2025-09-01 16:16:22 -0500

    Update MAINSI.java

--- src/proyectoaula/MAINSI.java
+++ src/proyectoaula/MAINSI.java
@@ -3,7 +3,7 @@ package proyectoaula;
 public class MAINSI {
 
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
+        System.out.println("Probando ando");
     }
     
 }
